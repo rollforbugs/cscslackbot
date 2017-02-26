fab139cee0d8756181385e8bd2a8438976b21273
joshuastevenson <stevenson.43@wright.edu>
2017-02-26 17:05:58 -0500

    Added uptime command

---
+++ plugins/uptime.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+import cscslackbot.slack as slack
+from cscslackbot.plugins import Command
+
+start_time = datetime.now()
+
+
+class UptimeCommand(Command):
+    name = 'uptime'
+    help_text = 'Provides the ammount of time the bot has been running'
+
+    def process_command(self, event, args):
+        now = datetime.now()
+        diff = now - start_time
+
+        resp = "Bot has been up since {} ({})"\
+            .format(start_time.strftime("%c"), str(diff).split(".")[0])
+        slack.send_message(event['channel'], resp)
