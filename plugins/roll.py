c5a2ca9ded935fefa589272d17d35680970a45db
joshuastevenson <stevenson.43@wright.edu>
2017-03-08 23:41:20 -0500

    Roll command clean up
    Reduced duplicated code. A minor performance fix.

--- plugins/roll.py
+++ plugins/roll.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 import math
 from random import randint, gauss
 
+from builtins import range
+
 import cscslackbot.slack as slack
 from cscslackbot.plugins import Command
 
@@ -40,46 +42,30 @@ class HelloCommand(Command):
             count = int(count)
             faces = int(faces)
             # Prevent abuse
+            msg = None
             if count > MAX_COUNT or faces > MAX_FACES:
-                slack.send_message(
-                    event['channel'],
-                    "Jesus christ calm your shit"
-                )
-                return
-            if faces > REASONABLE_FACES:
-                slack.send_message(
-                    event['channel'],
-                    "What do you picture these dice even look like?"
-                )
-                return
-            if count > REASONABLE_COUNT:
-                slack.send_message(
-                    event['channel'],
-                    "There's a limit to the number of dice I can fit in my hand"
-                )
-                return
-            if count == 0 or faces in (0, 1):
-                slack.send_message(
-                    event['channel'],
-                    "What possible use would rolling that have?"
-                )
-                return
-            if count < 0 or faces < 0:
-                slack.send_message(
-                    event['channel'],
-                    "No."
-                )
+                msg = "Jesus christ calm your shit"
+            elif faces > REASONABLE_FACES:
+                msg = "What do you picture these dice even look like?"
+            elif count > REASONABLE_COUNT:
+                msg = "There's a limit to the number of dice I can fit in my hand"
+            elif count == 0 or faces in (0, 1):
+                msg = "What possible use would rolling that have?"
+            elif count < 0 or faces < 0:
+                msg = "No."
+            if msg:
+                slack.send_message(event['channel'], msg)
                 return
 
             # Roll!
-            rolls = []
-            for i in range(count):
-                rolls.append(randint(1, faces))
+            rolls = [randint(1, faces) for i in range(count)]
 
             message = template.format(count, faces, sum(rolls))
             # Show specific rolls if it wouldn't take up too much space
-            if count > 1 and len(str(rolls)) < 150:
-                message += ' ({})'.format(str(rolls))
+            # Setting a max count would be more performant
+            roll_rep = str(rolls)
+            if count > 1 and len(roll_rep) < 150:
+                message += ' {}'.format(roll_rep)
 
             slack.send_message(event['channel'], message)
 
