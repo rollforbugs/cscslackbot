cececdc85edcb94a62f861e9bd2885c8fa1fc944
Jonathan Zentgraf <zx96dev@gmail.com>
2017-03-16 23:44:20 -0400

    Rename parse_command to process_event

--- cscslackbot/__init__.py
+++ cscslackbot/__init__.py
@@ -12,7 +12,7 @@ from cscslackbot.config import config, load_config, load_secrets
 logger = logging.getLogger(__name__)
 
 
-def parse_command(event):
+def process_event(event):
     plugins.plugins_process_event(event)
 
 
@@ -49,12 +49,12 @@ def run():
                     if 'user' not in event or event['user'] != slack.authed_user_id:
                         continue
 
-                parse_command(event)
+                process_event(event)
 
             # Don't hog the CPU busy-waiting
             time.sleep(0.1)
     except KeyboardInterrupt:
-        logger.info('Shutting down.')
+        logger.info('Shutting down')
 
 if __name__ == '__main__':
     run()
