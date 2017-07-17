9c873bb3eb42a7c594bf22a81fb16548c4784f79
Jonathan Zentgraf <zx96dev@gmail.com>
2017-07-17 19:45:39 -0400

    Make Slackbot class

--- cscslackbot/__init__.py
+++ cscslackbot/__init__.py
@@ -2,63 +2,72 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import logging
+import os
 import time
 
-import cscslackbot.plugins as plugins
 import cscslackbot.slack as slack
-from cscslackbot.config import get_config
 
-config = get_config(namespace='core')
-logger = logging.getLogger(__name__)
-
-
-def process_event(event):
-    if 'type' not in event:
-        logger.error('Received message without type key: {}'.format(event))
-        return
-    plugins.plugins_process_event(event)
+from cscslackbot.config import get_config, load_config, save_config
+from cscslackbot.plugins import Plugin
+from cscslackbot.utils.path import list_files
 
 
-def run():
-    logger.info('Starting bot')
+class Slackbot:
+    def __init__(self, config_path=None):
+        self.config_path = config_path
+        self.load_config()
+        self.config = get_config(namespace='core')
+        self.slack = slack
+        self.logger = logging.getLogger(__name__)
+        self.plugins = Plugin.plugins
 
-    if not slack.connect():
-        logger.error('Cannot connect to Slack. Please verify your token in the config.')
-        return
-    else:
-        logger.info('Connected to Slack as {} ({})'.format(slack.authed_user, slack.authed_user_id))
+    def load_config(self):
+        if os.path.isfile(self.config_path):
+            print('Flat config file: {}'.format(self.config_path))
+            load_config(self.config_path)
+        elif os.path.isdir(self.config_path):
+            print('Config directory: {}'.format(self.config_path))
+            for f in list_files(self.config_path):
+                rel = os.path.relpath(f, self.config_path)
+                print('Loading config file: {}'.format(rel))
+                namespace, ext = os.path.splitext(rel)
+                namespace = namespace.replace(os.path.sep, '.')
+                load_config(f, namespace=namespace)
 
-    plugins.load_plugins()
+    def process_events(self):
+        events = self.slack.get_events()
+        for event in events:
+            self.logger.debug(str(event))
 
-    try:
+            if self.config['debug_mode']:
+                # Whitelist #bottesting
+                if 'channel' not in event:
+                    continue
+                if event['channel'] not in ['C494WSTUL', '#bottesting']:
+                    continue
+                # Only respond to the developer when in debug mode
+                if 'user' not in event or event['user'] != self.slack.authed_user_id:
+                    continue
+
+            if 'type' not in event:
+                self.logger.error('Received message without type key: {}'.format(event))
+                continue
+
+            for p in self.plugins:
+                if hasattr(p, 'process_event'):
+                    p.process_event(event)
+
+    def run(self):
+        self.logger.info('Starting bot')
+
+        if not self.slack.connect():
+            self.logger.error('Cannot connect to Slack. Please verify your token in the config.')
+            return
+        else:
+            self.logger.info('Connected to Slack as {} ({})'.format(self.slack.authed_user, self.slack.authed_user_id))
+
         while True:
-            events = slack.get_events()
-            for event in events:
-                logger.info(str(event))
-
-                if config['debug_mode']:
-                    # Whitelist #bottesting
-                    if 'channel' not in event:
-                        continue
-                    if event['channel'] not in ('C494WSTUL', '#bottesting'):
-                        continue
-                    # Only respond to the developer when in debug mode
-                    if 'user' not in event or event['user'] != slack.authed_user_id:
-                        continue
-
-                process_event(event)
+            self.process_events()
 
             # Don't hog the CPU busy-waiting
-            if slack.mode == slack.SlackState.MODE_NORMAL:
-                time.sleep(0.1)
-            # INTERACTIVE calls input which blocks
-            elif slack.mode == slack.SlackState.MODE_SCRIPT:
-                logger.info("End of script")
-                break
-    except KeyboardInterrupt:
-        logger.info('Shutting down')
-    except:
-        logger.critical('Uncaught exception!', exc_info=True)
-        exit(-1)
-
-
+            time.sleep(0.1)
--- run.py
+++ run.py
@@ -1,38 +1,59 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import cscslackbot
-import cscslackbot.slack as slack
-from sys import argv, stderr
+
+import argparse
+import os
+
+from cscslackbot import Slackbot
+
+
+def valid_file(parser, arg):
+    if not os.path.isfile(arg):
+        parser.error('Cannot find file: `{}`'.format(arg))
+    else:
+        return os.path.realpath(arg)
+
+
+def valid_path(parser, arg):
+    if not os.path.exists(arg):
+        parser.error('Cannot find file or directory: `{}`'.format(arg))
+    else:
+        return os.path.realpath(arg)
 
 
 def main():
-    # Args: [mode [script_name]]
-    # python run.py
-    # python run.py INTERACTIVE
-    # python run.py SCRIPT tracked/quick_test_all.txt
+    """
+    Run a copy of the Slack bot.
 
-    # TODO help?
+    Usage: run.py [-h] [-i | -s SCRIPT]
+      Run normally:       python run.py
+      Display help:       python run.py -h
+      Test interactively: python run.py -i
+      Test with a script: python run.py -s script.txt
+    """
 
-    argc = len(argv)
-    if argc == 1:
-        slack.mode = slack.SlackState.MODE_NORMAL
+    parser = argparse.ArgumentParser(description='A Slack bot')
+    parser.add_argument('-c', '--config', type=lambda f: valid_file(parser, f),
+                        help='path to config file or directory')
+    mode_group = parser.add_mutually_exclusive_group()
+    mode_group.add_argument('-i', '--interactive', action='store_true',
+                            help='provide fake events interactively for testing')
+    mode_group.add_argument('-s', '--script', type=lambda f: valid_file(parser, f),
+                            help='a script of fake events for testing')
+    args = parser.parse_args()
+
+    config_path = None
+    if args.config:
+        config_path = args.config
     else:
-        state = argv[1]
-        if state not in slack.SlackState.MODE_OPTIONS:
-            print("Invalid parameter", file=stderr)
-            exit(1)
+        # Try default paths
+        if os.path.isfile('config.yml'):
+            config_path = 'config.yml'
+        elif os.path.isdir('config'):
+            config_path = 'config'
 
-        slack.mode = state
-
-        if state == slack.SlackState.MODE_SCRIPT:
-            if argc == 3:
-                slack.script_file = argv[2]
-            else:
-                print("file name needed", file=stderr)
-                exit(1)
-    ##
-
-    cscslackbot.run()
+    bot = Slackbot(config_path=config_path)
+    bot.run()
 
 
 if __name__ == "__main__":
